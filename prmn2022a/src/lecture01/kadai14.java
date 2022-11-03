2fb0bc90b797c78dc1ab4dbb278f338d9fce8fa4
[JumpeiHosakawa] <[b2212090@photon.chitose,ac,jp]>
2022-11-03 11:09:03 +0900

    kadai1-4

--- prmn2022a/src/lecture01/kadai14.java
+++ prmn2022a/src/lecture01/kadai14.java
@@ -1,10 +1,16 @@
 package lecture01;
 
 public class kadai14 {
-    int sum=0;
     public static void main(String[] args) {
+        int sum=0;
         int[] array = new int[100];
-        
+        for (int i=0;i<array.length;i++){
+            array[i]=i+1;
+            if(i%2==1){
+                sum=sum+array[i];
+            }
+        }
+        System.out.println(sum);
 
     }
 }
