9ce5f07a23db55ec0b4cf5e1a972ae5a070c59e5
[JumpeiHosakawa] <[b2212090@photon.chitose,ac,jp]>
2022-11-13 14:15:34 +0900

    Github上に表示するコメント文(例:プロジェクト作成)

---
+++ prmn2022a/src/lecture03/Exercise3_1.java
@@ -0,0 +1,11 @@
+package lecture03;
+
+import java.util.Scanner;
+
+public class Exercise3_1 {
+    public static void main(String[] args) {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("任意の文字列を入力してください。");
+        System.out.println(scanner.nextLine()+"と入力されました。");
+    }
+}
